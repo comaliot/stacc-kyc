8d7a895bd48d48c3b2500be6de662b3ee2fc8e66
comaliot <samuolis.rokas@gmail.com>
2022-11-12 20:13:32 +0100

    Styling changes

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import Button from '../Button/Button';
+import React, { useState } from 'react';
 import styles from './Input.module.css';
 
+const data = require('../../data/pep_small.json');
+
 export default function Input() {
+  const [value, setValue] = useState('');
+
+  const onChange = event => {
+    // gets the value from the input field and put inside the state
+    setValue(event.target.value);
+  };
+
+  const onSearch = searchTerm => {
+    setValue(searchTerm);
+    console.log('search', searchTerm);
+  };
+
   return (
     <>
       <input
@@ -10,8 +23,36 @@ export default function Input() {
         type="name"
         name="name"
         placeholder="f.eks Thomas"
+        value={value}
+        onChange={onChange}
       />
-      <Button />
+
+      <button onClick={() => onSearch(value)} className={styles.button}>
+        Search
+      </button>
+
+      <div className={styles.searchDropdown}>
+        {data
+          .filter(person => {
+            const searchTerm = value.toLowerCase();
+            const personName = person.name.toLowerCase();
+
+            return (
+              searchTerm &&
+              personName.startsWith(searchTerm) &&
+              personName !== searchTerm
+            );
+          })
+          .map(person => (
+            <div
+              onClick={() => onSearch(person.name)}
+              key={person.id}
+              className={styles.dropdownRow}
+            >
+              {person.name}
+            </div>
+          ))}
+      </div>
     </>
   );
 }
